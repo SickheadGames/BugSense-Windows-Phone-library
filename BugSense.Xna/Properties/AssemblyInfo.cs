8d497ef454c3461914570c0dc5cb2a46964cd40a
tomspilman <tom@sickheadgames.com>
2012-08-03 04:15:13 -0500

    Fixed assembly copyright.

--- BugSense.Xna/Properties/AssemblyInfo.cs
+++ BugSense.Xna/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 [assembly: AssemblyProduct("BugSense.Xna")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
